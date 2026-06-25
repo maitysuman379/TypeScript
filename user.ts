0f0459bdea6eba215255f1106b8dc2e411ffecee
maitysuman379 <maitysuman379@gmail.com>
2026-06-25 14:19:10 +0530

    first commit

---
+++ user.ts
@@ -0,0 +1,80 @@
+console.log('TypeScript')
+// var userName:string = "Suman Maity";
+// var age:number = 23;
+// var college:string = "Chitkara University";
+
+// console.log(userName)
+// console.log(age)
+// console.log(college)
+
+
+
+
+// var numArray:number[] = [1,2,3,4,5];
+// var numArray1:Array<Number> = [1,2,3,4,5];
+
+// console.log(numArray)
+// console.log(numArray1)
+
+
+
+
+// var names:string[] = ['Suman','Maity'];
+// var names1:Array<string> = ['Suman','Maity'];
+
+// console.log(names)
+// console.log(names1)
+
+//------------->>>               Tupels 
+// var Tup:[string,number] = ['suman',22];
+
+// console.log(Tup)
+
+
+//-------------------->>>>      Object
+// var obj:{name:string; age:number; isWrong:boolean} = {name:'Suman Maity',age:22,isWrong:false};
+
+// console.log(obj)
+
+
+
+// ---------->>>>>>>>>>>>>>    any
+// var a:any = 'Suman';
+// var b:any = 22;
+// var c:any = "Chitkara University";
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+
+// ---------->>>>>>>>>>>>>>    unknown
+// let input: unknown = "hello";
+
+// if(typeof(input)=='string'){
+//     console.log(input.toUpperCase())
+// }
+
+
+// ---------->>>>>>>>>>>>>>    void
+// function suman():void {
+//     console.log(suman)
+// }
+
+
+// ---------->>>>>>>>>>>>>>    never
+// function suman():never{
+//     console.log(suman)
+// }
+
+
+
+
+
+
+// ----------------------------  Number Data Type_____________________
+var num1:number = 10;
+var num2:number = 20;
+var total = num1 + num2;
+console.log(total)
